24e8c39734b47f94fddd9ca69cf0048a5345a22c
Suguen Lim <guen8903@gmail.com>
2026-03-18 14:47:36 +0900

    first commit

---
+++ my-bills-service/src/main/java/com/example/my_bill_service/MyBillServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.my_bill_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBillServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBillServiceApplication.class, args);
+	}
+
+}
