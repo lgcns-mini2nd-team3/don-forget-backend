a0fb762fe94b4d3dafbaebed9b592d64d9be6c1b
Suguen Lim <guen8903@gmail.com>
2026-03-23 22:41:27 +0900

    chore : 폴더 재배치

--- payment-service/src/main/java/com/example/payment_service/service/OpenFeignClient.java
+++ payment-service/src/main/java/com/example/payment_service/service/OpenFeignClient.java
@@ -4,8 +4,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.example.payment_service.domain.dto.InvoiceResponse;
+import com.example.payment_service.domain.dto.CreatePaymentResponse;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @FeignClient(name = "my-bill-service") // my-bill-service의 application.yml에서 설정한 spring.application.name
@@ -13,10 +14,5 @@ public interface OpenFeignClient {
 
     // 예: 오늘 발행 대상만 조회 (issueDay=오늘 날짜의 day-of-month)
     @GetMapping("/api/v1/my-bills/issue-targets")
-    List<InvoiceResponse> getIssueTargets(
-            @RequestParam("issueDay") int issueDay
-    );
-
-    @GetMapping("/api/v1/my-bills/get-invoices")
-    List<Long> getInvoicesByUserId(@RequestParam("userId") Long userId);
+    List<CreatePaymentResponse> getIssueTargets(@RequestParam("today") String today);
 }
